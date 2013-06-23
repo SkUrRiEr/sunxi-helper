b9244c55df1591e98b78a691dc055c32594b1bb8
Julian Calaby <julian.calaby@gmail.com>
2013-06-24 09:48:01 +1000

    Add memdumpBase
    Not having this broke everything.

---
+++ memdump/memdumpBase.php
@@ -0,0 +1,20 @@
+<?php
+
+class memdumpBase {
+	public $dram_clk;
+	public $dram_type;
+	public $dram_rank_num;
+	public $dram_chip_density;
+	public $dram_io_width;
+	public $dram_bus_width;
+	public $dram_cas;
+	public $dram_zq;
+	public $dram_odt_en;
+	public $dram_tpr0;
+	public $dram_tpr1;
+	public $dram_tpr2;
+	public $dram_tpr3;
+	public $dram_emr1;
+	public $dram_emr2;
+	public $dram_emr3;
+}
