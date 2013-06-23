d5108b0b96be8a6e3d1fbf451352f393ad1524a7
Julian Calaby <julian.calaby@gmail.com>
2013-06-24 09:51:04 +1000

    memdump/flatFile: Parse hexadecimal values for eventual comparisons

--- memdump/flatFile.php
+++ memdump/flatFile.php
@@ -5,11 +5,16 @@ include_once("memdump/memdumpBase.php");
 class flatFile extends memdumpBase {
 	public function __construct($fd) {
 		while($line = fgets($fd))
-			if( preg_match("/^([[:alnum:]_]*)\s*=\s*([[:digit:]abcdefx]*)$/", $line, $regs) ) {
-				$x = $regs[1];
+			if( preg_match("/^([[:alnum:]_]*)\s*=\s*([[:digit:]abcdefx]*)\s*$/", $line, $regs) ) {
+				$k = $regs[1];
 
-				if( property_exists($this, $x) )
-					$this->$x = $regs[2];
+				$v = $regs[2];
+
+				if( preg_match("/^0x([[:digit:]abcdef]*)$/", $v, $regs) )
+					$v = hexdec($regs[1]);
+
+				if( property_exists($this, $k) )
+					$this->$k = $v;
 			}
 	}
 }
